097f88dab543f85fe7032a96386da33374abc19f
raphsutti <neoster9@gmail.com>
2018-05-21 06:53:08 +1000

    conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 // function that returns array of books. Any file import this function will have this
 export default function() {
   return [
-    { title: "Harry Potter" },
-    { title: "Learning React" },
-    { title: "JavaScript is Awesome!" },
-    { title: "Pragmatic Programmer" }
+    { title: "Harry Potter", pages: 101 },
+    { title: "Learning React", pages: 39 },
+    { title: "JavaScript is Awesome!", pages: 85 },
+    { title: "Pragmatic Programmer", pages: 195 }
   ];
 }
